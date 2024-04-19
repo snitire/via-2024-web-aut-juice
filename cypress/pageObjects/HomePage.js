e5719348578e38550ef8a7f897990aff389ce33d
snitire <elvis.ritins@gmail.com>
2024-04-19 15:15:04 +0300

    Add new element selectors

--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -21,8 +21,24 @@ export class HomePage extends BasePage {
     return cy.get("button").contains("Login");
   }
 
-  static get accountMenu() {
-    return cy.get("[aria-label='Go to user profile']");
+  static get basketBtn() {
+    return cy.get("button").contains("Your Basket");
+  }
+
+  static get accountMenuUser() {
+    return cy.get("button[aria-label='Go to user profile']");
+  }
+
+  static get accountMenuOrders() {
+    return cy.get("button[aria-label='Show Orders and Payment Menu']");
+  }
+
+  static get accountMenuSavedAddresses() {
+    return cy.get("button[aria-label='Go to saved address page']");
+  }
+
+  static get accountMenuPaymentOptions() {
+    return cy.get("button[aria-label='Go to saved payment methods page']");
   }
 
   static get searchIcon() {
@@ -31,5 +47,18 @@ export class HomePage extends BasePage {
 
   static get searchInput() {
     return HomePage.searchIcon.find("input");
+  }
+
+  static searchFor(input) {
+    HomePage.searchIcon.click();
+    HomePage.searchInput.type(input + "{enter}");
+  }
+
+  static get itemsPerPage() {
+    return cy.get("mat-select[aria-label='Items per page:']")
+  }
+
+  static setItemsPerPageTo(number) {
+    cy.get(".mat-option-text").contains(number).click();
   }
 }
