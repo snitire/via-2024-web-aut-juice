f85055eca2fb5232a1f0074fa136bf5d131150a5
snitire <elvis.ritins@gmail.com>
2024-04-19 12:35:04 +0300

    Add required element selectors

--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -12,4 +12,16 @@ export class HomePage extends BasePage {
   static get meWantItButton() {
     return cy.get("[aria-label='dismiss cookie message']");
   }
+
+  static get accountBtn() {
+    return cy.get("button").contains("Account");
+  }
+
+  static get loginBtn() {
+    return cy.get("button").contains("Login");
+  }
+
+  static get accountMenu() {
+    return cy.get("[aria-label='Go to user profile']");
+  }
 }
