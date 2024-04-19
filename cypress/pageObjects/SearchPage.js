15bf3bd742e51729dba70fd33967147f20bd565e
snitire <elvis.ritins@gmail.com>
2024-04-19 15:15:27 +0300

    Add new element selectors

--- cypress/pageObjects/SearchPage.js
+++ cypress/pageObjects/SearchPage.js
@@ -10,10 +10,36 @@ export class SearchPage extends BasePage {
   }
 
   static findResultCardByName(productName) {
-    return cy.get(".item-name").contains(productName);
+    return cy.get(".item-name").contains(productName).parents(".ribbon-card");
   }
 
   static get openedCard() {
     return cy.get("app-product-details");
+  }
+
+  static closeCard() {
+    let closeBtn = cy.get("button").contains("Close");
+    closeBtn.click();
+  }
+
+  static get reviewPanelHeader() {
+    return cy.get("mat-expansion-panel-header");
+  }
+
+  static get allReviews() {
+    return cy.get(".review-text");
+  }
+
+  static get newReviewTextArea() {
+    return cy.get("[aria-label='Text field to review a product']");
+  }
+
+  static get newReviewSubmitBtn() {
+    return cy.get("#submitButton");
+  }
+
+  static addProductToBasket(name) {
+    let productCard = SearchPage.findResultCardByName(name);
+    productCard.find("button[aria-label='Add to Basket']").click();
   }
 }
