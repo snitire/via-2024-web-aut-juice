deb86a8a633c932ff13408af8896e8e42c63e78d
snitire <elvis.ritins@gmail.com>
2024-04-19 12:35:24 +0300

    Add required element selectors

--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -8,4 +8,20 @@ export class LoginPage extends BasePage {
   static get elementName() {
     return cy.get("elementSelector");
   }
+
+  static get emailField() {
+    return cy.get("#email");
+  }
+
+  static get passwordField() {
+    return cy.get("#password");
+  }
+
+  static get logInBtn() {
+    return cy.get("button").contains("Log in");
+  }
+
+  static get newUserBtn() {
+    return cy.get("a").contains("Not yet a customer?")
+  }
 }
