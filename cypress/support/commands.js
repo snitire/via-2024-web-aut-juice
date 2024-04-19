300b836d66dbda0f92f08191236d54f343311dec
snitire <elvis.ritins@gmail.com>
2024-04-19 12:35:38 +0300

    Add exception handling

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -50,3 +50,9 @@ Cypress.Commands.add("login", (username, password) => {
     }
   );
 });
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
