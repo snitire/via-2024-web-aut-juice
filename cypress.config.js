208f0db662c324c28d9149c5be6c25161ae460cb
snitire <elvis.ritins@gmail.com>
2024-04-19 12:33:47 +0300

    Add config option

--- cypress.config.js
+++ cypress.config.js
@@ -10,5 +10,6 @@ module.exports = defineConfig({
     },
     // Update this url (if necessary) to one that is used by juice-shop
     baseUrl: "http://127.0.0.1:3000",
+    testIsolation: true,
   },
 });
